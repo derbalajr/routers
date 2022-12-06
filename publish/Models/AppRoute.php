964854463419b8012294e65c46f751b19d24a430
omar.derbala <omar.derbala@acmesaico.net>
2022-12-06 12:53:36 +0200

    add route model

---
+++ publish/Models/AppRoute.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AppRoute extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['method', 'route','name','action'];
+
+    public function getDescriptionForEvent($eventName)
+    {
+        return __CLASS__ . " model has been {$eventName}";
+    }
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults();
+    }
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class)->withTimestamps();
+    }
+}
