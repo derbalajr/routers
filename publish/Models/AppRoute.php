9fb74754f87973e652258055706415643ec9706a
omar.derbala <omar.derbala@acmesaico.net>
2022-12-06 13:04:51 +0200

    edit route model

--- publish/Models/AppRoute.php
+++ publish/Models/AppRoute.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class AppRoute extends Model
 {
     use HasFactory;
+    use LogsActivity;
 
     protected $fillable = ['method', 'route','name','action'];
 
